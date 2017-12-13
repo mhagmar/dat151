e7c00a18d12c49fc3cf586bb132bd15edb781cea
Jesper Jaxing <jaxing@student.chalmers.se>
2017-12-13 11:20:12 +0100

    stud

--- lab3/haskell/Compiler.hs
+++ lab3/haskell/Compiler.hs
@@ -14,8 +14,9 @@ import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-import System.Process (callProcess)
+import System.Process (callProcess, callCommand)
 import System.FilePath.Posix (addExtension)
+import System.FilePath (takeFileName, takeDirectory)
 
 import Ann.Abs
 import Ann.Print
@@ -44,17 +45,20 @@ data St = St
   }
 
 -- | Variable contexts.
-type Cxt = TODO
-data TODO = TODO
+type Cxt = [Map Id Var]
+type Var = (Addr, Type)
 
 -- | Labels.
 newtype Label = L { theLabel :: Int }
   deriving (Eq, Enum)
 
+instance Show Label where
+    show (L i) = "l" ++ show i
+
 -- | Empty state.
 initSt :: St
 initSt = St
-  { cxt = TODO
+  { cxt = [Map.empty]
   , limitLocals   = 0
   , currentStack  = 0
   , limitStack    = 0
@@ -76,9 +80,20 @@ data Code
   | Return Type      -- ^ Return from method of type @Type@.
   | Call Fun         -- ^ Call function.
 
-  | Inc Type Addr Int -- ^ In/decrease variable by small number
+  -- | Inc Type Addr Int -- ^ In/decrease variable by small number
   | Add Type         -- ^ Add 2 top values of stack.
+  | Sub Type
+  | Mul Type
+  | Div Type
 
+  | IfEq Label
+  | IfLt Label
+  | IfLte Label
+  | IfGt Label
+  | IfGte Label
+  | GoTo Label
+
+  | Lab Label
 
 -- | Entry point.
 
@@ -86,14 +101,14 @@ compile :: String -> Program -> IO ()
 compile name prg@(PDefs defs) = do
   let sig = Map.fromList $
         builtin ++
-        map (\ def@(DFun _ f@(Id x) _ _ ) -> (f, Fun (Id $ name ++ "/" ++ x) $ funType def)) defs
-  let ((), w) = evalRWS (compileProgram name prg) sig initSt
+        map (\ def@(DFun _ f@(Id x) _ _ ) -> (f, Fun (Id $ (takeFileName name) ++ "/" ++ x) $ funType def)) defs
+  let ((), w) = evalRWS (compileProgram (takeFileName name) prg) sig initSt
   -- Print to stdout.
   mapM_ putStrLn w
   -- Write to .j file and call jasmin
   let jfile = addExtension name "j"
   writeFile jfile $ unlines w
-  callProcess "jasmin" [jfile]
+  callCommand ("jasmin -d " ++ (takeDirectory name) ++ " " ++ jfile)
 
 -- | Compiling the program.
 
@@ -139,7 +154,8 @@ compileFun def@(DFun t f args ss) = do
 
   -- prepare environment
   lab <- gets nextLabel
-  put initSt{ nextLabel = lab }
+  c   <- gets cxt
+  put initSt{ cxt = ((Map.empty):c),nextLabel = lab}
   mapM_ (\ (ADecl t' x) -> newVar x t') args
 
   -- compile statements
@@ -155,7 +171,7 @@ compileFun def@(DFun t f args ss) = do
 
   -- output code, indented by 2
   tell $ map (\ s -> if null s then s else "  " ++ s) w
-
+  tell ["  return"]
   -- function footer
   tell [ "", ".end method"]
 
@@ -182,7 +198,35 @@ compileStm s = do
       (a, _) <- lookupVar x
       emit (Store t a)
 
-    s -> nyi s
+    SDecls t ids -> do
+      mapM_ (\id -> newVar id t) ids
+
+    SWhile exp stm -> do
+        test <- getLabel
+        end <- getLabel
+        emit (Lab test)
+        compileExp exp
+        emit (IfEq end)
+        compileStm stm
+        emit (GoTo test)
+        emit (Lab end)
+
+
+    SBlock stms -> do
+        newBlock
+        mapM_ compileStm stms
+        exitBlock
+
+    SIfElse exp stm1 stm2 -> do
+        ifLabel <- getLabel
+        elseLabel <- getLabel
+        compileExp exp
+        emit (IfEq elseLabel)
+        compileStm stm1
+        emit (GoTo ifLabel)
+        emit (Lab elseLabel)
+        compileStm stm2
+        emit (Lab ifLabel)
 
 -- | Compiling and expression.
 
@@ -204,12 +248,169 @@ compileExp = \case
       emit (Store t a)
       emit (Load t a)
 
+    EPreDecr x -> do
+      (a, t) <- lookupVar x
+      emit (Load t a)
+      emit (IConst 1)
+      emit (Sub t)
+      emit (Store t a)
+      emit (Load t a)
+
+    EPostIncr x -> do
+      (a, t) <- lookupVar x
+      emit (Load t a)
+      emit (Load t a)
+      emit (IConst 1)
+      emit (Add t)
+      emit (Store t a)
+
+    EPostDecr x -> do
+      (a, t) <- lookupVar x
+      emit (Load t a)
+      emit (Load t a)
+      emit (IConst 1)
+      emit (Sub t)
+      emit (Store t a)
+
     EApp f es -> do
       mapM_ compileExp es
       sig <- ask
       let Just fun = Map.lookup f sig
       emit (Call fun)
 
+    EDiv t exp1 exp2 -> do
+      compileExp exp1
+      compileExp exp2
+      emit (Div t)
+
+    ETimes t exp1 exp2 -> do
+      compileExp exp1
+      compileExp exp2
+      emit (Mul t)
+
+    EPlus t exp1 exp2 -> do
+      compileExp exp1
+      compileExp exp2
+      emit (Add t)
+
+    EMinus t exp1 exp2 -> do
+      compileExp exp1
+      compileExp exp2
+      emit (Sub t)
+
+    ELt t exp1 exp2 -> do
+        true <- getLabel
+        end  <- getLabel
+        compileExp exp1
+        compileExp exp2
+        emit (Sub t)
+        emit (IfLt true)
+        emit (IConst 0)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 1)
+        emit (Lab end)
+
+
+    EGt t exp1 exp2 -> do
+        true <- getLabel
+        end  <- getLabel
+        compileExp exp1
+        compileExp exp2
+        emit (Sub t)
+        emit (IfGt true)
+        emit (IConst 0)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 1)
+        emit (Lab end)
+
+    ELtEq t exp1 exp2 -> do
+        true <- getLabel
+        end  <- getLabel
+        compileExp exp1
+        compileExp exp2
+        emit (Sub t)
+        emit (IfLte true)
+        emit (IConst 0)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 1)
+        emit (Lab end)
+
+    EGtEq t exp1 exp2 -> do
+        true <- getLabel
+        end  <- getLabel
+        compileExp exp1
+        compileExp exp2
+        emit (Sub t)
+        emit (IfGte true)
+        emit (IConst 0)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 1)
+        emit (Lab end)
+
+    EEq t exp1 exp2 -> do
+        true <- getLabel
+        end  <- getLabel
+        compileExp exp2
+        compileExp exp1
+        emit (Sub t)
+        emit (IfEq true)
+        emit (IConst 0)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 1)
+        emit (Lab end)
+
+    ENEq t exp1 exp2 -> do
+        true <- getLabel
+        end  <- getLabel
+        compileExp exp2
+        compileExp exp1
+        emit (Sub t)
+        emit (IfEq true)
+        emit (IConst 1)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 0)
+        emit (Lab end)
+
+    EAnd exp1 exp2 -> do
+        compileExp exp1
+        compileExp exp2
+        emit (Add Type_int)
+        emit (IConst 2)
+        true <- getLabel
+        end  <- getLabel
+        emit (Sub Type_int)
+        emit (IfEq true)
+        emit (IConst 0)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 1)
+        emit (Lab end)
+
+    EOr exp1 exp2 -> do
+        compileExp exp1
+        compileExp exp2
+        emit (Add Type_int)
+        emit (IConst 1)
+        true <- getLabel
+        end  <- getLabel
+        emit (Sub Type_int)
+        emit (IfLte true)
+        emit (IConst 0)
+        emit (GoTo end)
+        emit (Lab true)
+        emit (IConst 1)
+        emit (Lab end)
+
+    EAss (EId id) exp2 -> do
+        compileExp exp2
+        (a, t) <- lookupVar id
+        emit (Store t a)
 
     e -> nyi e
 
@@ -237,6 +438,10 @@ emit c = do
     Return t  -> decStack t
     Call f    -> decStack f
     Add t     -> decStack t
+    Sub t     -> decStack t
+    Mul t     -> decStack t
+    Div t     -> decStack t
+    s         -> return ()
 
 instance ToJVM Code where
   toJVM c = case c of
@@ -250,7 +455,18 @@ instance ToJVM Code where
 
     IConst i  -> "ldc " ++ show i
 
-    Add t        -> prefix t ++ "add"
+    Add t     -> prefix t ++ "add"
+    Sub t     -> prefix t ++ "sub"
+    Mul t     -> prefix t ++ "mul"
+    Div t     -> prefix t ++ "div"
+
+    Lab l     -> show l ++ ":"
+    IfEq l    -> "ifeq " ++ show l
+    IfLt l    -> "iflt " ++ show l
+    IfLte l   -> "ifle " ++ show l
+    IfGt l    -> "ifgt " ++ show l
+    IfGte l   -> "ifge " ++ show l
+    GoTo l    -> "goto " ++ show l
 
 -- | Type-prefix for JVM instructions.
 prefix :: Type -> String
@@ -323,7 +539,8 @@ decStack t = modStack (0 - size t)
 -- | Builtin-functions
 builtin :: [(Id, Fun)]
 builtin =
-  [ (Id "printInt"   , Fun (Id "Runtime/printInt"   ) $ FunType Type_void [Type_int])
+  [ (Id "printInt"   , Fun (Id "Runtime/printInt"   ) $ FunType Type_void [Type_int]),
+    (Id "readInt"    , Fun (Id "Runtime/readInt"    ) $ FunType Type_int [])
   ]
   -- TODO: more functions
 
@@ -356,13 +573,46 @@ instance ToJVM Label where
 ---------------------------------------------------------------------------
 
 newVar :: Id -> Type -> Compile ()
-newVar x t = fail $ "TODO: newVar"
+newVar x t = do
+    c:cs <- gets cxt
+    L i <- getLabel
+    ll <- gets limitLocals
+    modify $ \ st -> st {cxt = ((Map.insert x (i, t) c ):cs),
+                         limitLocals = ll + 1}
+
 
 -- | Return address and type of variable.
 lookupVar :: Id -> Compile (Addr, Type)
-lookupVar x = fail $ "TODO: lookupVar"
+lookupVar x = do
+    cs <- gets cxt
+    case lookupVar' x cs of
+        Just(addr, typ) -> return (addr, typ)
+        Nothing -> fail "No such variable"
+
+lookupVar' :: Id -> Cxt -> Maybe (Addr, Type)
+lookupVar' id [] = Nothing
+lookupVar' id (c:cs) = case Map.lookup id c of
+    Just(addr, typ) -> return (addr, typ)
+    Nothing -> lookupVar' id cs
 
 -- | Not yet implemented.
 
 nyi :: Print a => a -> b
 nyi t = error $ "Not yet implemented: compilation of: " ++ printTree t
+
+
+getLabel :: Compile (Label)
+getLabel = do
+    (L nexLabel) <- gets nextLabel
+    modify $ \ st -> st {nextLabel = (L (nexLabel + 1))}
+    return (L nexLabel)
+
+newBlock :: Compile ()
+newBlock = do
+    con <- gets cxt
+    modify $ \ st -> st {cxt = (Map.empty:con)}
+
+exitBlock :: Compile ()
+exitBlock = do
+    (c:con) <- gets cxt
+    modify $ \ st -> st {cxt = con}
--- lab3/haskell/test/test.cc
+++
@@ -1,5 +0,0 @@
-int main () {
-  int x = 0;
-  printInt(++x);
-  return x;
-}
